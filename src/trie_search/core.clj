f1eacbf0162f6a962cc8bc8c72521e8456af3bb8
Arun Mascarenhas <arunmascarenhas@yahoo.com>
2014-10-28 01:35:10 -0500

    Finished the match function. Cleaned up code. Added utility methods. Added sample word lists.

--- src/trie_search/core.clj
+++ src/trie_search/core.clj
@@ -1,14 +1,20 @@
 (ns trie-search.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
-(def trie {:head {}})
+;; The root node is :head
+;; An empty trie is {:head {}}
+;; The word termination marker is {:nil nil})
 
-(def termination-marker {:nil nil})
+(defn- file->line-seq [f]
+  (let [dict (with-open [r (io/reader f)]
+               (doall (line-seq r)))]
+    dict))
 
 (defn word->keyword-seq [word]
   (cons :head
         (map (comp keyword str)
-             (seq word))))
+             (seq (str/lower-case word)))))
 
 (defn add-word [t word]
   (assoc-in t
@@ -21,21 +27,36 @@
 (defn add-sentence [t sentence]
   (add-words t (str/split sentence #" ")))
 
-(defn- walk-helper [sub-t curr-word acc]
-  (cond 
-   (= sub-t termination-marker) (cons curr-word acc)
-   (contains? sub-t :nil) (recur (dissoc sub-t :nil)
-                                               curr-word
-                                               (cons curr-word acc))
-;;   :else (loop [t-keys (keys sub-t)
-;;                sub-t sub-t
-;;                curr-word curr-word
-;;                acc acc]
-;;(;; logic here))))
-   :else acc))
+(defn file->trie [file]
+  (loop [words (file->line-seq file)
+         t (make-empty-trie)]
+    (if (seq words)
+      (recur (rest words)
+             (add-word t (first words)))
+      t)))
 
-(defn walk [t word]
-  (let [sub-t (get-in t (word->keyword-seq word))]
-    (walk-helper sub-t word [])))
+(defn make-empty-trie []
+  {:head {}})
 
+(defn word->trie [word]
+  (add-word (make-empty-trie) word))
+
+(defn words->trie [words]
+  (add-words (make-empty-trie) words))
+
+(defn sentence->trie [sentence]
+  (add-sentence (make-empty-trie) sentence))
+
+(defn match [trie word]
+  (if (> 3 (count word))
+    []
+    (letfn [(search [node w acc]
+              (mapcat (fn [[k v]]
+                        (if (= :nil k)
+                          (cons w acc)
+                          (search v (str w (name k)) acc)))
+                      node))]
+      (search (get-in trie (word->keyword-seq word))
+              word
+              []))))
 
